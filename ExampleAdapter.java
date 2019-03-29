0e1b9dea4d31191cc3c3150098a3f59390a79c2f
beejay012 <48917589+beejay012@users.noreply.github.com>
2019-03-29 18:50:49 +0100

    Add files via upload

---
+++ ExampleAdapter.java
@@ -0,0 +1,132 @@
+package com.c.myapplication;
+
+import android.content.ClipData;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder> implements Filterable {
+    private Context mContext;
+    private ArrayList<ExampleItem> mExampleList;
+    private ArrayList<ExampleItem> mExampleListFull;
+
+    public ExampleAdapter(Context context, ArrayList<ExampleItem> exampleList) {
+        mContext = context;
+        mExampleList = exampleList;
+        mExampleListFull = new ArrayList<>(exampleList);
+    }
+
+    @Override
+    public ExampleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.example_item, parent, false);
+        return new ExampleViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ExampleViewHolder holder, final int position) {
+        ExampleItem currentItem = mExampleList.get(position);
+
+        final String imageUrl = currentItem.getImageUrl();
+        final String creatorName = currentItem.getCreator();
+        final int likeCount = currentItem.getLikeCount();
+        final int vues = currentItem.getVues();
+        final int comments = currentItem.getComments();
+
+
+        holder.mTextViewCreator.setText(creatorName);
+        holder.mTextViewLikes.setText("Likes: " + likeCount);
+        Picasso.with(mContext).load(imageUrl).fit().centerInside().into(holder.mImageView);
+
+        holder.view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(mContext, DescriptionActivity.class);
+                intent.putExtra("image_url", imageUrl);
+                intent.putExtra("image_name",creatorName);
+                intent.getIntExtra("image_like", likeCount);
+                intent.getIntExtra("image_vue", vues);
+                intent.getIntExtra("", comments);
+                mContext.startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mExampleList.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return exampleFilter;
+    }
+
+    private Filter exampleFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<ExampleItem> filteredList = new ArrayList<>();
+
+            if (constraint == null || constraint.length() == 0) {
+                filteredList.addAll(mExampleListFull);
+            } else {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+
+                for (ExampleItem item : mExampleListFull) {
+                    if (item.getCreator().toLowerCase().contains(filterPattern)) {
+                        filteredList.add(item);
+                    }
+                }
+            }
+
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            mExampleList.clear();
+            mExampleList.addAll((List) results.values);
+            notifyDataSetChanged();
+        }
+    };
+
+    public class ExampleViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mImageView;
+        public TextView mTextViewCreator;
+        public TextView mTextViewLikes;
+        public TextView mTextViewVues;
+        public TextView mTextViewComments;
+        public View view;
+
+
+        public ExampleViewHolder(View itemView) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.image_view);
+            mTextViewCreator = itemView.findViewById(R.id.text_view_creator);
+            mTextViewLikes = itemView.findViewById(R.id.text_view_likes);
+            mTextViewVues= itemView.findViewById(R.id.image_vue);
+            mTextViewComments=itemView.findViewById(R.id.image_commentaire);
+            view = itemView;
+        }
+    }
+}
+
